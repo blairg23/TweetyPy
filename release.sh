9656cefdde4f351fb2b38722725a4e7255ee99f0
Blairg23 <blair.gemmer@gmail.com>
2015-12-03 03:13:06 -0700

    Added release script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 git add --all
 git commit -m "$1"
-git push && git push --mirror https://github.com/blairg23/tweeter
+git push && git push --mirror https://github.com/blairg23/tweeter.git
 git status
